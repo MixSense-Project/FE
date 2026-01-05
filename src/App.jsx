71615ba26bc951c0cee9b7d169bf3dd33d596671
soyu <soyou5876@naver.com>
2026-01-06 01:19:01 +0900

    feat: library 페이지 구성 및 모달 구현 (#2)

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,18 @@ import Ai_Search from "./page/Ai_Search/Ai_Search";
 import Ai_Dj from "./page/Ai_Dj/Ai_Dj";
 import Library from "./page/Library/Library";
 import Mypage from "./page/Mypage/Mypage";
+import LibraryLikedSongs from "./page/Library/Library_likedsongs";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Splash/>}/>
-      <Route path="/home" element={<Home/>}/>
-      <Route path="/ai_search" element={<Ai_Search/>}/>
-      <Route path="/ai_dj" element={<Ai_Dj/>}/>
-      <Route path="/library" element={<Library/>}/>
-      <Route path="/mypage" element={<Mypage/>}/>
+      <Route path="/" element={<Splash />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/ai_search" element={<Ai_Search />} />
+      <Route path="/ai_dj" element={<Ai_Dj />} />
+      <Route path="/library" element={<Library />} />
+      <Route path="/library/liked" element={<LibraryLikedSongs />} />
+      <Route path="/mypage" element={<Mypage />} />
     </Routes>
   );
 }
