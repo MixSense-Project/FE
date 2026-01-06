46850ecbc7677d28f0257cdf1b3e23cb343c9140
soyu <soyou5876@naver.com>
2026-01-07 02:00:47 +0900

    feat: components-library-myplaylist 구성 (#2)

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,20 @@ import Home_trending_now from "./page/Home/Home_trending_now";
 import Ai_Search from "./page/Ai_Search/Ai_Search";
 import Ai_Dj from "./page/Ai_Dj/Ai_Dj";
 import Library from "./page/Library/Library";
-import Mypage from "./page/Mypage/Mypage";
+import Mypage from "./page/Home/Mypage";
 import LibraryLikedSongs from "./page/Library/Library_likedsongs";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Splash/>}/>
-      <Route path="/home" element={<Home/>}/>
-      <Route path="/home_search" element={<Home_search/>}/>
-      <Route path="/home_trending_now" element={<Home_trending_now/>}/>
-      <Route path="/ai_search" element={<Ai_Search/>}/>
-      <Route path="/ai_dj" element={<Ai_Dj/>}/>
-      <Route path="/library" element={<Library/>}/>
-      <Route path="/mypage" element={<Mypage/>}/>
+      <Route path="/" element={<Splash />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/home_search" element={<Home_search />} />
+      <Route path="/home_trending_now" element={<Home_trending_now />} />
+      <Route path="/ai_search" element={<Ai_Search />} />
+      <Route path="/ai_dj" element={<Ai_Dj />} />
+      <Route path="/library" element={<Library />} />
+      <Route path="/mypage" element={<Mypage />} />
       <Route path="/" element={<Splash />} />
       <Route path="/home" element={<Home />} />
       <Route path="/ai_search" element={<Ai_Search />} />
--- src/page/Library/Library_likedsongs.jsx
+++ src/page/Library/Library_likedsongs.jsx
@@ -7,23 +7,24 @@ import plus_btn from '../../assets/img/library/plus_btn.svg'
 const Library_likedsongs = () => {
     const navigate = useNavigate()
     return (
-        <div className='container librarylikedsongs_wrap'>
-            <Header title={"Library"} />
+        <div className='librarylikedsongs_wrap'>
+            <div className="container">
+                <Header title={"Library"} />
 
-            <div className="category">
-                <button className="playlist"
-                    onClick={() => navigate('/library')}>
-                    <p>Playlist</p>
-                </button>
+                <div className="category">
+                    <button className="playlist"
+                        onClick={() => navigate('/library')}>
+                        <p>Playlist</p>
+                    </button>
 
-                <button
-                    className="liked"
-                    onClick={() => navigate('/library/liked')}
-                >
-                    <p>Liked Songs</p>
-                </button>
+                    <button
+                        className="liked"
+                        onClick={() => navigate('/library/liked')}
+                    >
+                        <p>Liked Songs</p>
+                    </button>
+                </div>
             </div>
-
             <Nav />
         </div>
     )
