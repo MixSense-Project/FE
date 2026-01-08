b6ea43747dcf986dff9a086e33f24f1f9d317cde
kimseongyeon <seongyeon0810@naver.com>
2026-01-08 22:58:30 +0900

    Nav 컬러 변경

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,15 @@
 import { NavLink } from "react-router-dom";
 import dj_g from "../assets/img/nav/dj_g.svg";
-import dj_p from "../assets/img/nav/dj_p.svg";
 import home_g from "../assets/img/nav/home_g.svg";
-import home_p from "../assets/img/nav/home_p.svg";
 import library_g from "../assets/img/nav/library_g.svg";
-import library_p from "../assets/img/nav/library_p.svg";
 import search_g from "../assets/img/nav/search_g.svg";
-import search_p from "../assets/img/nav/search_p.svg";
+
+import home_w from '../assets/img/Nav/home_w.svg'
+import search_w from '../assets/img/Nav/aisearch_w.svg'
+import library_w from '../assets/img/Nav/library_w.svg'
+import dj_w from '../assets/img/Nav/aidj_w.svg'
+
+
 
 const Nav = () => {
   const isHomeActive = location.pathname === "/home" || location.pathname === "/home_search" || location.pathname === "/home_trending_now";
@@ -20,7 +23,7 @@ const Nav = () => {
       >
         {({ isActive }) => (
           <>
-            <img src={isHomeActive ? home_p : home_g} className="nav_icon" alt="" />
+            <img src={isHomeActive ? home_w : home_g} className="nav_icon" alt="" />
             <p className="nav_p">Home</p>
           </>
         )}
@@ -32,7 +35,7 @@ const Nav = () => {
       >
         {({ isActive }) => (
           <>
-            <img src={isSearchActive ? search_p : search_g} className="nav_icon" alt="" />
+            <img src={isSearchActive ? search_w : search_g} className="nav_icon" alt="" />
             <p className="nav_p">AI Search</p>
           </>
         )}
@@ -44,7 +47,7 @@ const Nav = () => {
       >
         {({ isActive }) => (
           <>
-            <img src={isActive ? dj_p : dj_g} className="nav_icon" alt="" />
+            <img src={isActive ? dj_w : dj_g} className="nav_icon" alt="" />
             <p className="nav_p">AI DJ</p>
           </>
         )}
@@ -57,7 +60,7 @@ const Nav = () => {
         {({ isActive }) => (
           <>
             <img
-              src={isActive ? library_p : library_g}
+              src={isActive ? library_w : library_g}
               className="nav_icon"
               alt=""
             />
