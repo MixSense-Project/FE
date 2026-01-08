c40a0426895210741ff69411ea3efeb48575f63d
kimseongyeon <seongyeon0810@naver.com>
2026-01-08 16:30:49 +0900

    AISearch 페이지 구성

--- src/page/Ai_Search/Ai_Search.jsx
+++ src/page/Ai_Search/Ai_Search.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import Header from '../../components/Header'
 import Nav from '../../components/Nav'
+import logo_img from '../../assets/img/logo.svg'
+import chat_btn from '../../assets/img/AISearch/chat_btn.svg'
 
 const Ai_Search = () => {
   return (
-    <div className="aisearch_wrap">
+    <div className="Ai_Search_Wrap">
       <div className='container'>
         <Header title={"AI Search"}/>
+        <header>
+            <img src={logo_img} alt="" />
+            <p>MixSense AI</p>
+        </header>
+        <div className="chat">
+            <button className="chat_btn">
+                <img src={chat_btn} alt="" />
+            </button>
+            <input type="text" />
+        </div>
       </div>
       <Nav/>
     </div>
