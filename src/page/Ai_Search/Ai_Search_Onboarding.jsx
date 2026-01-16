29d37536963a05f15ccf7c2858c9b9f55e9b4993
soyu <soyou5876@naver.com>
2026-01-17 01:32:41 +0900

    fix: margin deatail 수정

--- src/page/Ai_Search/Ai_Search_Onboarding.jsx
+++ src/page/Ai_Search/Ai_Search_Onboarding.jsx
@@ -4,38 +4,41 @@ import Nav from '../../components/Nav'
 import Header from '../../components/Header'
 import logo_img from '../../assets/img/logo.svg'
 import chat_btn from '../../assets/img/AISearch/chat_btn.svg'
-import graphic_img from '../../assets/img/AISearch/graphic.png'
+import main_img from '../../assets/img/AISearch/aisearch_bgc.png'
 
 const Ai_Search_Onboarding = () => {
-  return (
-    <div className="Ai_Search_Onboarding_wrap">
-        <div className="container">
-            <Header title={"AI Search"}/>
-            <header>
-                <img src={logo_img} alt="" />
-                <p>MixSense AI</p>
-            </header>
-            <main>
-                <h1>What song are you 
-                <br />looking for?</h1>
-                <p>
-                    Describe the characteristics or mood 
-                    <br />of the music you are looking for.
-                </p>
-                <img src={graphic_img} alt="" />
-            </main>
-            <Link to='/ai_search'>
-                <div className="chat">
-                    <button className="chat_btn">
-                        <img src={chat_btn} alt="" />
-                    </button>
-                    <input type="text" />
-                </div>
-            </Link>
+    return (
+        <div className="Ai_Search_Onboarding_wrap">
+            <div className="container">
+                <Header title={"AI Search"} />
+                <header>
+                    <img src={logo_img} alt="" />
+                    <p>MixSense AI</p>
+                </header>
+                <main>
+                    <h1>What song are you
+                        <br />looking for?</h1>
+                    <p>
+                        Describe the characteristics or mood
+                        <br />of the music you are looking for.
+                    </p>
+                    <img src={main_img} alt="" />
+                </main>
+                <Link to='/ai_search'>
+                    <div className="chat">
+                        <button className="chat_btn">
+                            <img src={chat_btn} alt="" />
+                        </button>
+                        <input
+                            type="text"
+                            placeholder="Please explain the song you want to find"
+                        />
+                    </div>
+                </Link>
+            </div>
+            <Nav />
         </div>
-        <Nav/>
-    </div>
-  )
+    )
 }
 
 export default Ai_Search_Onboarding
